6acbdf80d7f90c8b281ec33c9e4e57b73661be2f
SwipeDan <swipedbrain@gmail.com>
2015-04-26 17:48:02 -0300

    hehe
    hehe

---
+++ Trinket Cooldown Tracker/Trinket Cooldown Tracker/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+using System.Drawing;
+using Color = System.Drawing.Color;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Trinket_Cooldown_Tracker
+{
+  class Program
+  {
+
+    private static Menu Config;
+
+    public static Items.Item Trinket = new Items.Item(3340, 600f);
+
+    public static Obj_AI_Hero Player
+    {
+      get
+      {
+        return ObjectManager.Player;
+      }
+    }
+    static void Main(string[] args)
+    {
+      CustomEvents.Game.OnGameLoad += OnGameLoad;
+      Drawing.OnDraw += OnDraw;
+    }
+
+    private static void OnGameLoad(EventArgs args)
+    {
+      Game.PrintChat("Trinket Tracker by DanZ and Drunkenninja loaded");
+      Config = new Menu("Trinket Tracker", "Trinket_Tracker", true);
+      Config.AddSubMenu(new Menu("On", "On"));
+      Config.SubMenu("On").AddItem(new MenuItem("On", "On")).SetValue(true);
+
+      Config.AddToMainMenu();
+      Game.OnUpdate += OnGameUpdate;
+
+    }
+
+
+    private static void OnDraw(EventArgs args)
+    {
+    }
+
+    private static void OnGameUpdate(EventArgs args)
+    {
+
+      if (Config.Item("On").GetValue<bool>())
+      {
+        if (Trinket.IsOwned())
+        {
+          if (Trinket.IsReady())
+          {
+
+            Drawing.DrawText(Player.HPBarPosition.X +30, Player.HPBarPosition.Y - 30, Color.LawnGreen, "Trinket Up!");
+          }
+          else
+          {
+            Drawing.DrawText(Player.HPBarPosition.X + 30, Player.HPBarPosition.Y - 30, Color.Red, "Trinket Down!");
+
+          }
+        }
+      }
+    }
+  }
+}
