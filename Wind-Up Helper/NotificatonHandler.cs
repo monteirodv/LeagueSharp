4325519b05624c421989f42578c36e5d1eaa2e6f
SwipeDan <swipedbrain@gmail.com>
2015-05-06 18:31:20 -0300

    Releaseyay

---
+++ Wind-Up Helper/NotificatonHandler.cs
@@ -0,0 +1,27 @@
+﻿using LeagueSharp.Common;
+using SharpDX;
+
+namespace Wind_Up_Helper
+{
+  class NotificationHandler
+  {
+    private static Notification _modeNotificationHandler;
+
+    public static void Update()
+    {
+      var text = "Recommended Windup: " + Program.windup;
+
+      if (_modeNotificationHandler == null)
+      {
+        _modeNotificationHandler = new Notification(text)
+        {
+          TextColor = new ColorBGRA(124, 252, 0, 255)
+        };
+        Notifications.AddNotification("By DanZ and Drunkenninja");
+        Notifications.AddNotification(_modeNotificationHandler);
+      }
+
+      _modeNotificationHandler.Text = text;
+    }
+  }
+}
---
+++ Wind-Up Helper/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+using System.Drawing;
+using Color = System.Drawing.Color;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Wind_Up_Helper
+{
+  class Program
+  {
+    public static float windup;
+
+    private static Menu Config;
+
+    private static Orbwalking.Orbwalker Orbwalker;
+
+    public static Obj_AI_Hero Player
+    {
+      get
+      {
+        return ObjectManager.Player;
+      }
+    }
+    static void Main(string[] args)
+    {
+      CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+    }
+    static void Game_OnGameLoad(EventArgs args)
+    {
+     // Notifications.AddNotification("Wind-Up Helper by DanZ and Drunkenninja loaded!", 1000);
+      Config = new Menu("Wind-Up", "winduphelper", true);
+      Config.AddSubMenu(new Menu("On", "On"));
+      Config.AddSubMenu(new Menu("Orbwalking", "Orbwalking"));
+      Orbwalker = new Orbwalking.Orbwalker(Config.SubMenu("Orbwalking"));
+      Config.SubMenu("On").AddItem(new MenuItem("On", "On")).SetValue(true);
+      Config.AddToMainMenu();
+
+      Game.OnUpdate += OnGameUpdate;
+      Drawing.OnDraw += OnDraw;
+
+    }
+    private static void OnGameUpdate(EventArgs args)
+    {
+      NotificationHandler.Update();
+
+      if (Config.Item("On").GetValue<bool>())
+      {
+        {
+          windup = Game.Ping / 1.5f;
+        }
+      }
+    }
+    private static void OnDraw(EventArgs args)
+    {
+      //Drawing.DrawText(10, 10, Color.Red, windup.ToString());
+    }
+  }
+}
+
